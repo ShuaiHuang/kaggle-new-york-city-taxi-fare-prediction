08232c30f81fa3881941cc3ea8609aafdf3844f2
Shuai_Huang <shuaihuang.sjtu@gmail.com>
2018-09-01 22:06:08 +0800

    revise scripts for data cleaning

--- code/clean-dataset.py
+++ code/clean-dataset.py
@@ -3,6 +3,7 @@ import os
 import re
 import logging
 import argparse
+import numpy as np
 import pandas as pd
 import datetime as dt
 
@@ -17,26 +18,70 @@ def get_chunk_files(input_path, reg_exp):
     return chunk_file_list
 
 
-def parse_date_time(input_file_path, output_file_dir=None):
-    path, filename = os.path.split(input_file_path)
-    if output_file_dir is None:
-        cleaned_file_path = os.path.join(path, 'cleaned_' + filename)
+def read_data_frame_from_file(input_file_path, input_data_formation=None):
+    if input_data_formation == 'feather':
+        df = pd.read_feather(input_file_path, nthreads=6)
     else:
-        cleaned_file_path = os.path.join(output_file_dir, 'cleaned_' + filename)
+        df = pd.read_csv(input_file_path)
 
-    if not os.path.exists(cleaned_file_path):
-        chunk_df = pd.read_csv(file_path)
-        chunk_df['pickup_timezone'] = chunk_df['pickup_datetime'].str.extract(r'\d+-\d+\-\d+\s\d+:\d+:\d+\s([A-Z]{3})',
-                                                                              expand=False)
-        chunk_df['pickup_datetime_obj'] = chunk_df['pickup_datetime'].map(lambda x: dt.datetime.strptime(x, '%Y-%m-%d %H:%M:%S UTC'))
-        chunk_df['pickup_year'] = chunk_df['pickup_datetime_obj'].map(lambda x: x.year)
-        chunk_df['pickup_month'] = chunk_df['pickup_datetime_obj'].map(lambda x: x.month)
-        chunk_df['pickup_day'] = chunk_df['pickup_datetime_obj'].map(lambda x: x.day)
-        chunk_df['pickup_hour'] = chunk_df['pickup_datetime_obj'].map(lambda x: x.hour)
-        chunk_df['pickup_minute'] = chunk_df['pickup_datetime_obj'].map(lambda x: x.minute)
-        chunk_df['pickup_second'] = chunk_df['pickup_datetime_obj'].map(lambda x: x.second)
-        chunk_df['pickup_weekday'] = chunk_df['pickup_datetime_obj'].map(lambda x: x.weekday())
-        chunk_df.to_csv(cleaned_file_path, index=False)
+    return df
+
+
+def write_data_frame_to_file(data_frame, output_file_path, output_data_formation=None):
+    if output_data_formation == 'feather':
+        data_frame.to_feather(output_file_path)
+    else:
+        data_frame.to_csv(output_file_path, index=False)
+
+
+def get_output_file_path(input_file_path, output_file_dir, output_data_formation=None):
+    _, filename = os.path.split(input_file_path)
+
+    if output_data_formation == 'feather':
+        filename_base, _ = os.path.splitext(filename)
+        filename = filename_base + '.feather'
+
+    output_file_path = os.path.join(output_file_dir, filename)
+    return output_file_path
+
+
+def parse_date_time(data_frame):
+    data_frame['pickup_timezone'] = data_frame['pickup_datetime'].str.extract(r'\d+-\d+\-\d+\s\d+:\d+:\d+\s([A-Z]{3})', expand=False)
+    data_frame['pickup_datetime_obj'] = data_frame['pickup_datetime'].map(lambda x: dt.datetime.strptime(x, '%Y-%m-%d %H:%M:%S UTC'))
+    data_frame['pickup_year'] = data_frame['pickup_datetime_obj'].map(lambda x: x.year)
+    data_frame['pickup_month'] = data_frame['pickup_datetime_obj'].map(lambda x: x.month)
+    data_frame['pickup_day'] = data_frame['pickup_datetime_obj'].map(lambda x: x.day)
+    data_frame['pickup_hour'] = data_frame['pickup_datetime_obj'].map(lambda x: x.hour)
+    data_frame['pickup_minute'] = data_frame['pickup_datetime_obj'].map(lambda x: x.minute)
+    data_frame['pickup_second'] = data_frame['pickup_datetime_obj'].map(lambda x: x.second)
+    data_frame['pickup_weekday'] = data_frame['pickup_datetime_obj'].map(lambda x: x.weekday())
+    return data_frame
+
+
+def calculate_distance(data_frame):
+    data_frame['pickup_dropoff_distance'] = data_frame.apply(
+        lambda row: haversine_distance(row['pickup_latitude'], row['pickup_longitude'], row['dropoff_latitude'], row['dropoff_longitude']),
+        axis=1)
+    return data_frame
+
+def haversine_distance(lat1, long1, lat2, long2):
+    R = 6371  # radius of earth in kilometers
+    # R = 3959 #radius of earth in miles
+    phi1 = np.radians(lat1)
+    phi2 = np.radians(lat2)
+
+    delta_phi = np.radians(lat2 - lat1)
+    delta_lambda = np.radians(long2 - long1)
+
+    # a = sin²((φB - φA)/2) + cos φA . cos φB . sin²((λB - λA)/2)
+    a = np.sin(delta_phi / 2.0) ** 2 + np.cos(phi1) * np.cos(phi2) * np.sin(delta_lambda / 2.0) ** 2
+
+    # c = 2 * atan2( √a, √(1−a) )
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
+
+    # d = R*c
+    d = (R * c)  # in kilometers
+    return d
 
 
 if __name__ == '__main__':
@@ -54,28 +99,31 @@ if __name__ == '__main__':
     arg_parser.add_argument('--processing_data_dir',
                             type=str,
                             default='data-for-processing')
-    arg_parser.add_argument('--test_file',
+    arg_parser.add_argument('--input_data_formation',
                             type=str,
-                            default='test.csv')
+                            default='csv')
+    arg_parser.add_argument('--output_data_formation',
+                            type=str,
+                            default='feather')
     FLAGS, _ = arg_parser.parse_known_args()
 
     start_time = dt.datetime.now()
     end_time = dt.datetime.now()
 
-
     data_dir = os.path.join(FLAGS.prj_dir, FLAGS.data_dir, FLAGS.processing_data_dir)
-    chunk_files = get_chunk_files(data_dir, 'chunk')
+    if FLAGS.input_data_formation == 'feather':
+        reg_exp = r'chunk.*\.feather|test.feather'
+    else:
+        reg_exp = r'chunk.*\.csv|test.csv'
+    chunk_files = get_chunk_files(data_dir, reg_exp)
     logging.debug(chunk_files)
     for file in chunk_files:
-        file_path = os.path.join(FLAGS.prj_dir, FLAGS.data_dir, FLAGS.processing_data_dir, file)
+        file_path = os.path.join(data_dir, file)
+        output_file_path = get_output_file_path(file_path, data_dir, FLAGS.output_data_formation)
+        df = read_data_frame_from_file(file_path, FLAGS.input_data_formation)
         logging.debug('cleaning %s'%(file_path,))
-        parse_date_time(file_path)
+        df = parse_date_time(df)
+        df = calculate_distance(df)
+        write_data_frame_to_file(df, output_file_path, FLAGS.output_data_formation)
         end_time = dt.datetime.now()
         logging.debug('done in %s'%(end_time-start_time,))
-
-    test_file_path = os.path.join(FLAGS.prj_dir, FLAGS.data_dir, FLAGS.raw_data_dir, FLAGS.test_file)
-    cleaned_test_file_dir = os.path.join(FLAGS.prj_dir, FLAGS.data_dir, FLAGS.processing_data_dir)
-    logging.debug('cleaning %s'%(test_file_path,))
-    parse_date_time(test_file_path, cleaned_test_file_dir)
-    end_time = dt.datetime.now()
-    logging.debug('done in %s' % (end_time - start_time,))
--- code/split-dataset-into-chunks.py
+++ code/split-dataset-into-chunks.py
@@ -3,6 +3,8 @@ import os
 import logging
 import argparse
 import pandas as pd
+import datetime as dt
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
@@ -19,29 +21,49 @@ if __name__ == '__main__':
     arg_parser.add_argument('--train_data',
                             type=str,
                             default='train.csv')
+    arg_parser.add_argument('--test_data',
+                            type=str,
+                            default='test.csv')
+    arg_parser.add_argument('--output_data_formation',
+                            type=str,
+                            default='feather')
     arg_parser.add_argument('--chunk_size',
                             type=int,
                             default=5000000)
     FLAGS, _ = arg_parser.parse_known_args()
 
+    raw_data_dir = os.path.join(FLAGS.data_path, FLAGS.input_dir)
     raw_data_path = os.path.join(FLAGS.data_path, FLAGS.input_dir, FLAGS.train_data)
+    test_data_path = os.path.join(raw_data_dir, FLAGS.test_data)
+    output_data_dir = os.path.join(FLAGS.data_path, FLAGS.output_dir)
+    start_time = dt.datetime.now()
 
-    columns_type = {
-        'key': 'str',
-        'fare_amount': 'float32',
-        'pickup_datetime': 'str',
-        'pickup_longitude': 'float32',
-        'pickup_latitude': 'float32',
-        'dropoff_longitude': 'float32',
-        'dropoff_latitude': 'float32',
-        'passenger_count': 'uint8'
-    }
-    columns_key = columns_type.keys()
-
-    csv_reader = pd.read_csv(raw_data_path, chunksize=FLAGS.chunk_size, usecols=columns_key, dtype=columns_type)
+    csv_reader = pd.read_csv(raw_data_path, chunksize=FLAGS.chunk_size)
     for index, chunk_df in enumerate(csv_reader):
-        chunk_file_name = 'chunk_%03d_%s'%(index, FLAGS.train_data)
-        chunk_file_path = os.path.join(FLAGS.data_path, FLAGS.output_dir, chunk_file_name)
+        if FLAGS.output_data_formation == 'feather':
+            _, chunk_file_name = os.path.split(raw_data_path)
+            chunk_file_name_base, _ = os.path.splitext(chunk_file_name)
+            chunk_file_name = 'chunk_%03d_%s.feather'%(index, chunk_file_name_base)
+        else:
+            chunk_file_name = 'chunk_%03d_%s'%(index, FLAGS.train_data)
+
+        chunk_file_path = os.path.join(output_data_dir, chunk_file_name)
         logging.debug('saving %s'%(chunk_file_name))
-        chunk_df.to_csv(chunk_file_path, index=False)
-        logging.debug('done!')
+
+        if FLAGS.output_data_formation == 'feather':
+            chunk_df = chunk_df.reset_index()
+            chunk_df.to_feather(chunk_file_path)
+        else:
+            chunk_df.to_csv(chunk_file_path, index=False)
+        end_time = dt.datetime.now()
+        logging.debug('done in time %s'%(end_time-start_time,))
+
+    if FLAGS.output_data_formation == 'feather':
+        output_filename_base, _ = os.path.splitext(FLAGS.test_data)
+        output_filename = '%s.feather'%(output_filename_base,)
+        output_file_path = os.path.join(output_data_dir, output_filename)
+        df = pd.read_csv(test_data_path)
+        logging.debug('saving %s' % (output_filename))
+        df.to_feather(output_file_path)
+        end_time = dt.datetime.now()
+        logging.debug('done in time %s' % (end_time - start_time,))
